63d555edcafc5133684e012c91f2b8b4f3f78727
SoutrikBandyopadhyay <soutrik.band@gmail.com>
2017-08-29 18:30:57 +0530

    Structs

--- index.c
+++ index.c
@@ -8,9 +8,42 @@
 
 // Object oriented code = best code
 
+typedef struct _core{
+
+} Core;
+
+typedef struct _window{
+
+} Window;
+
+typedef struct _yoke{
+
+} Yoke;
+
+typedef struct _lv{
+
+} LVwindingData;
+
+typedef struct _hv{
+
+} HVwindingData;
 
 typedef struct _transformer{
   float KVA;
+  float Vp; //Primary HV side Voltage(in KV)
+  float Vs; //Secondary LV side Volatge(in KV)
+  float Bm; //Max Flux density
+  float phase; // Number of phases
+  float frequency; //Obvio
+
+
+
+
+  Core C;
+  Window W;
+  Yoke Y;
+  LVwindingData LV;
+  HVwindingData HV;
 
 
 } Transformer;

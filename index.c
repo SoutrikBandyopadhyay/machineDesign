4fbc45d4e3d8ac4dac272b599be2427e579fc93b
SoutrikBandyopadhyay <soutrik.band@gmail.com>
2017-08-29 17:17:59 +0530

    Using Struct for transformer

--- index.c
+++ index.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 
 
 #define pi 3.141592653589
+#define muo 0.000001256
+
+// Object oriented code = best code
+
+
+typedef struct _transformer{
+  float KVA;
+
+
+} Transformer;
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
